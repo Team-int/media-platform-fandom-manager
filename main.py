ac9ae0794dd335f6496bdd4cf39348c549cdb110
CONTROL_D <48048511+copecone@users.noreply.github.com>
2021-03-21 14:29:46 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ async def add_role(ctx, role: discord.Role, user: discord.Member):
                 await user.add_roles(role)
                 await ctx.send(f"성공적으로 역할 {role.mention}이(가) {user.mention}에게 추가되었습니다")
             except commands.errors.CommandInvokeError:
-                print('이 역할은 추가할 수 없습니다')
+                await ctx.send('이 역할은 추가할 수 없습니다')
         except commands.errors.RoleNotFound:
-            print('이 역할은 존재하지 않습니다')
+            await ctx.send('이 역할은 존재하지 않습니다')
 
 @client.command()
 async def remove_role(ctx, role: discord.Role, user: discord.Member):
@@ -32,9 +32,9 @@ async def remove_role(ctx, role: discord.Role, user: discord.Member):
                 await user.remove_roles(role)
                 await ctx.send(f"성공적으로 역할 {role.mention}이(가) {user.mention}에게서 제거되었습니다")
             except ommands.errors.CommandInvokeError:
-                print('이 역할은 제거할 수 없습니다')
+                await ctx.send('이 역할은 제거할 수 없습니다')
         except commands.errors.RoleNotFound:
-            print('이 역할은 존재하지 않습니다')
+            await ctx.send('이 역할은 존재하지 않습니다')
 
 @client.command()
 async def add_youtube_watcher(ctx, user: discord.Member):
@@ -62,9 +62,9 @@ async def add_youtube_watcher(ctx, user: discord.Member):
         target_role = discord.utils.find(lambda r: r.name == youtuber_name + ' 팬', ctx.message.server.roles)
         if not target_role in user.roles:
             await user.add_roles(target_role)
-            print('해당 유저를 당신의 팬으로 등록하였습니다')
+            await ctx.send('해당 유저를 당신의 팬으로 등록하였습니다')
         else:
-            print('해당 유저는 이미 당신의 팬으로 등록되어 있습니다')
+            await ctx.send('해당 유저는 이미 당신의 팬으로 등록되어 있습니다')
 
 @client.command()
 async def remove_youtube_watcher(ctx, user: discord.Member):
@@ -92,9 +92,9 @@ async def remove_youtube_watcher(ctx, user: discord.Member):
         target_role = discord.utils.find(lambda r: r.name == youtuber_name + ' 팬', ctx.message.server.roles)
         if target_role in user.roles:
             await user.remove_roles(target_role)
-            print('해당 유저를 당신의 팬에서 박탈하였습니다')
+            await ctx.send('해당 유저를 당신의 팬에서 박탈하였습니다')
         else:
-            print('해당 유저는 당신의 팬으로 등록되어 있지 않습니다')
+            await ctx.send('해당 유저는 당신의 팬으로 등록되어 있지 않습니다')
 
 youtuber_names = ['메이데이', '망토토']
 #Invite Link = https://discord.com/oauth2/authorize?client_id=822293801526624306&permissions=8&scope=bot
